3617a4d34c578a04e592f75f9b8938ec24455f93
yuza-plan <yuza.plan@gmail.com>
2018-08-03 00:08:36 +0900

    change /config name

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-require_once(__DIR__ . "/config.php");
+require_once(__DIR__ . "/config/config.php");
 require_once(__DIR__ . "/functions.php");
 require_once(__DIR__ . "/Todo.php");
 
